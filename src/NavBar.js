e0c3825a3fbf84eb526fab748669f54e97421c48
IvyAkeru <66743922+IvyAkeru@users.noreply.github.com>
2020-07-11 13:10:39 +0100

    Updates

---
+++ src/NavBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from './Button';
+import logo from './logo.svg';
+import Jumbotron from './Jumbotron';
+
+const NavBar = () => {
+
+    const promptLogin = () => {
+        window.location = "https://www.myapp.com/login"
+    }
+
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <Link className="navbar-brand" to="/">
+                <img src={logo} width="30" height="30" 
+                className="d-inline-block align-top" 
+                alt="" 
+                loading="lazy"/>
+                Bootstrap
+            </Link>
+
+            <div style={{display: 'flex'}}>
+                <Link
+                to="/login"
+                className="btn btn-primary">
+                    Log In
+                </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
